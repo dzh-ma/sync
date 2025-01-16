2cf651acd89e55803145016ae065ea6448397bbc
dzhma <dzhma@proton.me>
2025-01-16 23:29:14 +0400

    Further optimizations for user registration

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,10 +1,21 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-# db = client["project_database"]
-db = client.smart_home
-users_collection = db["users"]
+# Loading MongoDB URI from envvars
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+
+# Initialize MongoDB client
+try:
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.smart_home
+    users_collection = db["users"]
+except Exception as e:
+    raise ConnectionError(f"Failed to connect to MongoDB: {e}") from e
 
 async def init_db():
-    # Any necessary setup tasks (like creating indexes)
-    users_collection.create_index("email", unique = True)
+    """Initialize the database, such as creating necessary indexes."""
+    try:
+        users_collection.create_index("email", unique = True)
+        print("Database initialized successfully.")
+    except Exception as e:
+        raise RuntimeError(f"Error during database initialization: {e}") from e
