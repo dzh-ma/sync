30ee175606edfa1cf691ac951b17ba7a473c4e9b
dzhma <dzhma@proton.me>
2025-01-16 11:19:56 +0400

    general improvements

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["project_database"]
+# db = client["project_database"]
+db = client.smart_home
 users_collection = db["users"]
 
-def init_db():
+async def init_db():
     # Any necessary setup tasks (like creating indexes)
-    users_collection.create_index("email", unique=True)
+    users_collection.create_index("email", unique = True)
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from bson import ObjectId
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
 class UserResponse(BaseModel):
+    id: str
     email: EmailStr
     is_verified: bool
     created_at: datetime
+
+    class Config:
+        orm_mode = True
