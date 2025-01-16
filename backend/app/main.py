782f85f6cdce62499bdea61516486e616bd37eb2
dzhma <dzhma@proton.me>
2025-01-16 11:19:56 +0400

    general improvements

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,36 @@
+"""This module acts as a middlepoint between the database and the frontend."""
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.user_routes import router as user_router
 from app.db.database import init_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app_context: FastAPI):
+    '''Defines application lifespan event handler'''
+    await init_db()
+    app_context.state.custom_attribute = "value"    # Placeholder
+    yield
+    print("Application is shutting down.")
 
-# Initialize database
-@app.on_event("startup")
-async def startup_event():
-    init_db()
+app = FastAPI(lifespan = lifespan)
+
+# Frontend communication
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 # Include routers
-app.include_router(user_router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(user_router, prefix = "/api/v1/users", tags = ["Users"])
 
 # Root endpoint
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Smart Home Backend"}
+    '''Startup message.'''
+    return {"message": "Welcome to the Sync Smart Home"}
--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -1,25 +1,36 @@
+"""This module routes data to the database from registration"""
+from datetime import datetime, timezone
+
 from fastapi import APIRouter, HTTPException
+
 from app.models.user import UserCreate, UserResponse
 from app.db.database import users_collection
 from app.core.security import hash_password
-from datetime import datetime
 
 router = APIRouter()
 
-@router.post("/register", response_model=UserResponse)
+@router.post("/register", response_model = UserResponse)
 async def register_user(user: UserCreate):
+    '''Registers user to the database.'''
+
+    # Email existance check
     if users_collection.find_one({"email": user.email}):
-        raise HTTPException(status_code=400, detail="Email already registered.")
-    
+        raise HTTPException(status_code = 400, detail = "Email already registered.")
+
+    # Data that will be collected
     user_data = {
         "email": user.email,
         "password_hash": hash_password(user.password),
         "is_verified": False,
-        "created_at": datetime.utcnow(),
-        "updated_at": datetime.utcnow(),
+        "created_at": datetime.now(timezone.utc),
+        "updated_at": datetime.now(timezone.utc),
     }
-    users_collection.insert_one(user_data)
+
+    # Database user insert
+    result = users_collection.insert_one(user_data)
+
     return {
+        "id": str(result.inserted_id),
         "email": user.email,
         "is_verified": False,
         "created_at": user_data["created_at"]
