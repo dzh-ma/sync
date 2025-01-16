f4426c1e7489c1b7421af110bb23419097def67b
dzhma <dzhma@proton.me>
2025-01-17 00:12:47 +0400

    Updated backend, need to fix testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,31 +1,41 @@
 """This module acts as a middlepoint between the database and the frontend."""
+import os
+import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 from app.routes.user_routes import router as user_router
 from app.db.database import init_db
 
+# Configure logging
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Environment variables
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "*").split(",")
+
 @asynccontextmanager
 async def lifespan(app_context: FastAPI):
-    '''Defines application lifespan event handler'''
+    """Defines application lifespan event handler"""
     try:
         await init_db()
         app_context.state.custom_attribute = "value"    # Placeholder
-        print("Application startup complete.")
+        logger.info("Application startup complete.")
         yield
     except Exception as e:
-        print(f"Failed to initialize the database: {e}")
+        logger.error("Failed to initialize the database: %e.")
         raise e
     finally:
-        print("Application is shutting down.")
+        logger.info("Application is shutting down.")
 
 app = FastAPI(lifespan = lifespan)
 
 # Frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],      # Change `"*"` to specific origins in production
+    allow_origins = ALLOWED_ORIGINS,      # Change `"*"` to specific origins/domain in production
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
@@ -34,7 +44,12 @@ app.add_middleware(
 # Include routers
 app.include_router(user_router, prefix = "/api/v1/users", tags = ["Users"])
 
-@app.get("/", response_model = dict)
-def read_root() -> dict:
-    '''Root endpoint.'''
-    return {"message": "Welcome to the Sync Smart Home."}
+# Root response moedl
+class RootResponse(BaseModel):
+    message: str
+
+@app.get("/", response_model = RootResponse)
+def read_root() -> RootResponse:
+    """Root endpoint."""
+    return RootResponse(message = "Welcome to the Sync Smart Home.")
+    # return {"message": "Welcome to the Sync Smart Home."}
