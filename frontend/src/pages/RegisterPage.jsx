271b2e96662d87eb9d768c46845bbe43bfdc1e8c
dzh-ma <dzhma@proton.me>
2025-02-23 22:47:37 +0400

    adjusted frontend

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -7,28 +7,42 @@ const RegisterPage = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [role, setRole] = useState();
-  const [error, setError] = useState();
+  const [errors, setErrors] = useState();
   const [message, setMessage] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setErrors([]);        // clear previous errors
     try {
-      await axiosInstance.post("/users/registration", { email, password, role });
+      await axiosInstance.post("/users/register", { email, password, role });
       setMessage("Registration successful! Please log in.");
       navigate("/login");
     } catch (err) {
-      setError(
-        err.response?.data?.detail || "Registration failed, please try again."
-      );
-      console.error(err);
+      if (err.response && err.response.data.detail) {
+        const validationErrors = err.response.data.detail.map(
+          (item) => item.msg
+        );
+        setErrors(validationErrors);
+      } else {
+        setErrors(["Registration failed, please try again."]);
+      }
+      console.errors(err);
     }
   }
 
   return (
     <div>
       <h2>Register</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
       {message && <p style={{ color: "green" }}>{message}</p>}
+      {Array.isArray(errors) && errors.length > 0 ? (
+        <ul style={{ color: 'red' }}>
+          {errors.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      ) : (
+          errors && <p style={{ color: 'red' }}>{errors}</p>
+        )}
       <form onSubmit={handleSubmit}>
         <div>
           <label>Email:</label>
@@ -47,6 +61,11 @@ const RegisterPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
+          <br />
+          <small>
+            Password must be at least 8 characters long, contain at least 1 number,
+            1 letter (both uppercase & lowercase) & 1 special character
+          </small>
         </div>
         <div>
           <label>Role:</label>
